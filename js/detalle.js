f1f8b76888d7d546ae31f620cf663a400b6ab099
Alexa-2k <109705749+Alexa-2k@users.noreply.github.com>
2024-05-31 23:32:03 -0300

    Add files via upload

---
+++ js/detalle.js
@@ -0,0 +1,151 @@
+/* DETALLE PELICULA - PRUEBA*/
+//var idpeli = `${pelicula.id}`;
+//console.log(idpeli);
+//document.addEventListener("DOMContentLoaded", () => {
+  const apiUrl ="https://api.themoviedb.org/3/movie/popular?api_key=615cdfacf7dd0263ec1fcea8ee4352f6&language=es-MX";
+    //const getMovieIdFromUrl = () => {
+   // const params = new URLSearchParams(window.location.search);
+  //  return params.get("id");
+  //  console.log(params);
+ // };
+
+  //const movieId = Number(getMovieIdFromUrl());
+ const movieId = 823464;
+  console.log(movieId);
+
+  const fetchItem = async () => {
+    try {
+      console.log(`${apiUrl}/${movieId}`);
+      const response = await fetch(`${apiUrl}/${movieId}`);
+
+      if (!response.ok) {
+        window.location.href = "./notfound.html";
+        throw new Error("Error en la solicitud: " + response.statusText);
+      }
+      const movie = await response.json();
+
+      return movie;
+    } catch (error) {
+      console.error("Hubo un problema con la operación fetch:", error);
+      return [];
+    }
+  };
+
+  const btnMenu = document.querySelector(".btn-menu");
+  const menuItems = document.querySelector(".menu-items");
+  const btnBrand = document.querySelector(".brand");
+
+  // captura datos para ver si ya inicio session o no
+  const logOutButton = document.querySelector("#log-out");
+  const logInButton = document.querySelector("#log-in");
+  const adminButton = document.querySelector("#admin");
+  const registraseButton = document.querySelector("#registrarse");
+  var isLogged = localStorage.getItem("user");
+
+  console.log(isLogged);
+
+  // para mostrar o iniciar Session o el Logout
+  if (!isLogged) {
+    logOutButton.style.display = "none";
+    logInButton.style.display = "block";
+    registraseButton.style.display = "block";
+    adminButton.style.display = "none";
+  } else {
+    logOutButton.style.display = "block";
+    logInButton.style.display = "none";
+    registraseButton.style.display = "none";
+    adminButton.style.display = "block";
+  }
+
+  // para el menu hamburgesa
+  if (btnMenu) {
+    btnMenu.addEventListener("click", () => {
+      menuItems.classList.toggle("show");
+    });
+    menuItems.addEventListener("click", () => {
+      menuItems.classList.toggle("show");
+    });
+    btnBrand.addEventListener("click", () => {
+      menuItems.classList.toggle("show");
+    });
+  }
+
+  if (logOutButton) {
+    logOutButton.addEventListener("click", logout);
+  }
+
+  function logout() {
+    localStorage.removeItem("user");
+  }
+
+  function convertDateFormat(dateString) {
+    const [year, month, day] = dateString.split("-");
+    return `${day}-${month}-${year}`;
+  }
+
+  const titulo = document.querySelector("#titulo");
+  const overview = document.querySelector("#overview");
+  const realease = document.querySelector("#realease");
+  const runtime = document.querySelector("#runtime");
+  const genres = document.querySelector("#genres");
+  const crew = document.querySelector("#crew");
+  const poster = document.querySelector("#poster");
+  const statusData = document.querySelector(".status-data");
+  const languageData = document.querySelector(".language-data");
+  const budgetData = document.querySelector(".budget-data");
+  const revenueData = document.querySelector(".revenue-data");
+  const video = document.querySelector("#video");
+  const detailsHeader = document.querySelector(".details-header");
+
+  function formatNumber(number) {
+    // Utiliza el método toLocaleString con las opciones adecuadas para el formato deseado
+    return number.toLocaleString("es-ES", {
+      minimumFractionDigits: 2, // Asegura que siempre haya al menos 2 decimales
+      maximumFractionDigits: 2, // Limita a 2 decimales
+    });
+  }
+
+  // Función para obtener datos de la API
+
+  const renderItem = (movie) => {
+    const newImageUrl = movie.backdrop_path; // Reemplaza esto con la URL de la nueva imagen
+    detailsHeader.style.backgroundImage = `url('${newImageUrl}')`;
+    titulo.innerHTML = movie.title;
+    overview.innerHTML = movie.overview;
+    runtime.innerHTML = `${movie.runtime} min`;
+    realease.innerHTML = convertDateFormat(movie.release_date);
+    genres.innerHTML = movie.genres.map((genre) => genre.name).join(", ");
+
+    crew.innerHTML = "";
+    movie.crew.forEach((person) => {
+      const movieElement = document.createElement("div");
+      movieElement.innerHTML = `
+           <h3>${person.name}</h3>
+           <p>${person.job}</p>
+        `;
+      crew.appendChild(movieElement);
+    });
+
+    poster.innerHTML = `<img src="${movie.poster_path}" alt="${movie.title}"/>`;
+    statusData.innerHTML = movie.status;
+    languageData.innerHTML = movie.original_language;
+    budgetData.innerHTML = `$  ${formatNumber(movie.budget)}`;
+    revenueData.innerHTML = `$  ${formatNumber(movie.revenue)}`;
+
+    video.innerHTML = `
+    <iframe src="https://www.youtube.com/embed/${movie.video}" title="${movie.title}"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    `;
+  };
+
+  const loadPage = async () => {
+    const movie = await fetchItem();
+
+    renderItem(movie);
+  };
+
+  loadPage();
+});
+
--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const pager = document.querySelector("#paginar");
 
 
 
+
 let pagina = 1;
 
 btnAnterior.addEventListener("click", () => {
@@ -35,8 +36,7 @@ btnSiguiente.addEventListener("click", () => {
   pager.innerHTML = `Página: ${pagina}`;
   cargarPeliculas()
   window.location.href = "#tendencias";
-    // 
-    // llamar a la funcion que cargue las paginas de las peliculas desde la API e ir al inicio de la sección de Tendencias
+      // llamar a la funcion que cargue las paginas de las peliculas desde la API e ir al inicio de la sección de Tendencias
 
   });
 
@@ -46,7 +46,7 @@ const cargarPeliculas = async () => {
 
 try {
     //cargar en una variable el fetch de la API
-    const respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=615cdfacf7dd0263ec1fcea8ee4352f6&language=es-MX&page=${pagina}`)
+    const respuesta = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=615cdfacf7dd0263ec1fcea8ee4352f6&language=es-MX&page=${pagina}`)
     //verificar la respuesta en consola
     console.log(respuesta);
 
@@ -74,22 +74,18 @@ try {
         contenedor.innerHTML = peliculas;
         
        
-    } // Si  hay error 404, mensaje
-    else if (respuesta.status === 404) {
-        console.log("error 404 nos vemos en otro lugar");
-    }
+    } else if (respuesta.status === 404) {console.log("error 404 nos vemos en otro lugar");
+    } else if(respuesta.status === 401) {console.log("No tiene la api_key correcta");}
+
 //otros errores: mensaje de error por consola
-} catch(error) {
-    console.log(error.message);
-    }
+  } catch(error) {console.log("BOOM! Something exploded"); }
 }
 
 //Llamar a la funcion
 cargarPeliculas();
 
-
 /*==================================================================*/
-/* DETALLE PELICULA - PRUEBA*/
+/* DETALLE PELICULA */
 function fetchMovieDetails(movieId) {
         fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=615cdfacf7dd0263ec1fcea8ee4352f6&language=es-MX`)
            .then(response => response.json())
@@ -98,6 +94,60 @@ function fetchMovieDetails(movieId) {
                 document.querySelector('.peli-title').textContent = data.title;
                 document.querySelector('.peli-descripcion').textContent = data.overview;
                 document.querySelector('.peli-estreno').textContent = `Estreno: ${new Date(data.release_date).getFullYear()}`;
+                document.querySelector('.peli-clasificacion').textContent = `Clasificación: ${data.vote_average}`;
+                document.querySelector('.peli-duracion').textContent = `Duración: ${data.runtime} minutos`;
+                document.querySelector('.peli-trailer').href = `https://www.youtube.com/watch?v=${data.videos.results[0].key}`;
+                document.querySelector('.peli-reparto').textContent = `Reparto: ${data.credits.cast[0].name}`;
+                document.querySelector('.peli-director').textContent = `Director: ${data.credits.crew[0].name}`;
+                document.querySelector('.peli-genero').textContent = `Género: ${data.genres[0].name}`;
+                document.querySelector('.peli-idioma').textContent = `Idioma: ${data.spoken_languages[0].name}`;
+                document.querySelector('.peli-pais').textContent = `País: ${data.production_countries[0].name}`;
+                
             })
            .catch(error => console.error('Error:', error));
     }
+
+
+ /* ========= PRUEBA ACLAMADAS =============*/ 
+const cargarAclamadas = async (ids) => {
+  const apiKey = '615cdfacf7dd0263ec1fcea8ee4352f6'; // Asegúrate de usar tu propia clave API
+  const contAclamadas = document.querySelector('#aclamadas-main'); 
+
+  try {
+    for (let id of ids) {
+      const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&language=en-US`;
+      const resp = await fetch(url);
+
+      if (resp.status === 200) {
+        const datos = await resp.json();
+
+        // Formatear los datos de la película como HTML
+        const html = `
+          <div class="peli-card aclamada">
+            <a href="./detalle.html?id=${datos.id}">
+              <div class="card-img">
+                <img class="peli-img" src="https://image.tmdb.org/t/p/w500${datos.poster_path}" alt="${datos.title}">
+              </div>
+              <div class="card-title">
+                <h3 class= "card-title">${datos.title}</h3>
+              </div>
+            </a>
+          </div>
+        `;
+
+        // Agregar el HTML al contenedor
+        contAclamadas.innerHTML += html;
+      } else if (resp.status === 404) {
+        console.log(`Error 404 para la película con ID ${id}`);
+      } else {
+        console.error(`Error desconocido para la película con ID ${id}`);
+      }
+    }
+  } catch (error) {
+    console.error('Error al cargar películas:', error.message);
+  }
+};
+
+// Llamar a la función con tus IDs específicos
+const ids = [497, 155, 49026, 218, 105, 165, 196, 27205, 2277, 603, 16869, 36657, 1726, 157336, 238, 240, 242];
+cargarAclamadas(ids);
